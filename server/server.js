2857d25abc8bc02124869741185cc7decfade06a
syed muneeb bukhari <muneebbukhari02@gmail.com>
2019-08-23 22:45:03 +0500

    heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const path=require('path');
 const express=require('express');
+const port = process.env.PORT || 3000;
 const pubpath=path.join(__dirname , '../public');
 var app=express();
 app.use(express.static(pubpath));
-app.listen(3000,()=>{console.log('3000')});
+app.listen(port,()=>{console.log('3000')});
